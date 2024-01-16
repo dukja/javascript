2156e986ee9161b79aa7f5d21ed05413d5325e8b
dukja <100657522+dukja@users.noreply.github.com>
2024-01-16 12:44:37 +0900

    file name

--- Type.js
+++ Type.js
@@ -4,13 +4,14 @@ console.log(typeof "text")
 
 console.log(typeof null)
 
-//*protoType Chain
+//"protoType Chain"
 function MyClass(name){
   this.name = name
 }
 const A={
   name : "Class"
 }
+
 const B = new MyClass("Class")
 console.log(A instanceof MyClass)
 console.log(B instanceof MyClass)
@@ -27,57 +28,3 @@ console.log(date instanceof Object)
 
 //*prototype
 console.log(Object.prototype.toString.call(date))
-
-
-console.log(null == 0)
-
-
-!null
-!!null
-null === true
-!null === true
-null + 123
-
-
-!undefined
-undefined+123
-
-//== eq, type casting
-//eqeqeq
-const a = 0
-a == 0
-0 ===false
-Number(a) === 0
-
-
-'1' == 1
-1 == true
-0 == false
-
-
-//strict type castion
-11+'text'
-!!'text'
-!!''
-parseInt('9.999', 10)
-
-String(11+'text')
-Boolean(!!'text')
-!!''
-Number('11')
-
-parseInt('9.999', 10)
-
-//*explicit
-isNaN(123)
-Number.isNaN(123)
-
-//*boundary
-//min, begin, first, prefix, argument order
-
-
-
-
-
-
-
